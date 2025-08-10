8bb366a8e043cc9ac577a277c60f971bd0b4592a
Maya Ella Mampf <zrezlon@gmail.com>
2025-08-10 11:59:35 +0200

    automated commit for update

--- update.fish
+++ update.fish
@@ -4,8 +4,13 @@
 export confdir="$HOME/Projects/nixos-dots"
 
 cd $HOME
-if ls -la | grep -i '.gtkrc'
-  rm ./.gtkrc-2.0
+if ls -la | grep -i '.gtkrc-2.0'
+  if ls -la | grep -i '.gtkrc-2.0.bk'
+    rm ./.gtkrc-2.0.bk
+  end
+  if ls -la | grep -i '.gtkrc-2.0'
+    rm ./.gtkrc-2.0
+  end
 end
 cd $confdir
 git add .
