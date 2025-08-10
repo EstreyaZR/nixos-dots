1da746fdbbdfa116f440cfc36da211d67a0cca75
Maya Ella Mampf <zrezlon@gmail.com>
2025-08-10 12:57:14 +0200

    automated commit for update

--- flake.nix
+++ flake.nix
@@ -46,6 +46,8 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.maya = ./users/maya-home.nix;
+
+          estreya.desktop.enable = true;
         }
       ];
     };
