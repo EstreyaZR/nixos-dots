c84e9a97c998ab5eea6517122f8f241b0b5d5cef
EstreyaZR <nina.ella.ayar@gmail.com>
2025-08-20 09:32:00 +0200

    added more desktop support

---
+++ modules/desktop/cosmic.nix
@@ -0,0 +1,38 @@
+{pkgs, lib,config}: let cfg = config.estreya.desktop.cosmic in {
+  options.estreya.desktop.cosmic = {
+    enable = lib.mkEnableOption "enable cosmic";
+    default = false;
+  };
+
+  config = lib.mkIf cfg.enable {
+    services = {
+      desktopManager.cosmic.enable = true;
+      displayManager.cosmic-greeter.enable = true;
+    };
+    environment.systemPackages = with pkgs; [
+      cosmic-term
+      cosmic-bg
+      cosmic-osd
+      cosmic-edit
+      cosmic-comp
+      cosmic-store
+      cosmic-randr
+      cosmic-panel
+      cosmic-icons
+      cosmic-files
+      cosmic-player
+      cosmic-applet
+      cosmic-settings
+      cosmic-launcher
+      cosmic-protocols
+      cosmic-wallpapers
+      cosmic-screenshots
+      cosmic-notifications
+      cosmic-applibrary
+    ];
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-cosmic ];
+    };
+  };
+}
