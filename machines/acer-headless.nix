77240bac0a20fe1a510ba84ec8c1b6e6f0ec61c2
EstreyaZR <nina.ella.ayar@gmail.com>
2025-08-22 09:24:38 +0200

    automated commit for update

--- machines/acer-headless.nix
+++ machines/acer-headless.nix
@@ -30,12 +30,12 @@
   hardware.nvidia = {
     prime = {
       sync.enable = true;
-      nvidiaBusId = "PCI:1@0:0.0";
-      intelBusId = "PCI:0@0:2.0";
+      nvidiaBusId = "PCI:1@0:0:0";
+      intelBusId = "PCI:0@0:2:0";
     };
     powerManagement.enable = false;
     modesetting.enable = true;
-    open = false;
+    open = true;
   };
 
   hardware.nvidia-container-toolkit.enable = true;
